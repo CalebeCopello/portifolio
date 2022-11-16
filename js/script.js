a712d0d9ea51cefd2118f062b633554812463930
CalebeCopello <calebecopello@gmail.com>
2022-11-16 07:57:16 -0300

    Javascript
    Mostando conteúdo com JavaScript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showHome() {
+    document.getElementById('welcome-div').style.display = "inline"
+    document.getElementById('sobre-div').style.display = "none"
+}
+function showSobreMim() {
+    document.getElementById('sobre-div').style.display = "inline"
+    document.getElementById('welcome-div').style.display = "none"
+}
