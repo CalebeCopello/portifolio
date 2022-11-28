12c5e2c5447a64a08815affe6182959eff4c6961
CalebeCopello <calebecopello@gmail.com>
2022-11-27 21:11:12 -0300

    Update 27/11
    Troca de cores (temas), mudança função "show" no JavaScript.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-function showHome() {
-    document.getElementById('welcome-div').style.display = "inline"
+function show(a) {
+    document.getElementById('home-div').style.display = "none"
     document.getElementById('sobre-div').style.display = "none"
-}
-function showSobreMim() {
-    document.getElementById('sobre-div').style.display = "inline"
-    document.getElementById('welcome-div').style.display = "none"
+    document.getElementById('post-div').style.display = "none"
+    document.getElementById('projeto-div').style.display = "none"
+    document.getElementById('contato-div').style.display = "none"
+    switch(a) {
+        case 'home':
+            document.getElementById('home-div').style.display = "inline";
+            break;
+        case 'sobre':
+            document.getElementById('sobre-div').style.display = "inline";
+            break;
+        case 'post':
+            document.getElementById('post-div').style.display = "inline";
+            break;
+        case 'projeto':
+            document.getElementById('projeto-div').style.display = "inline";
+            break;
+        case 'contato':
+            document.getElementById('contato-div').style.display = "inline";
+    }
 }
