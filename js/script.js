41eba97fed3807fe6b86e83f55a2ccdc1356620e
CalebeCopello <calebecopello@gmail.com>
2022-12-02 10:02:26 -0300

    Temas
    Sistema de temas implementado

--- js/script.js
+++ js/script.js
@@ -21,8 +21,28 @@ function show(a) {
             document.getElementById('contato').style.display = "inline";
     }
 }
-function tema() {
+function tema(tema) {
     var getRoot = document.querySelector(':root')
-    var selectRoot = window.getComputedStyle(getRoot)
-    window.alert(selectRoot.getPropertyValue('--cor-base'))
+    switch(tema){
+        case 'glaucous':
+            getRoot.style.setProperty('--cor-base', 'rgba(100,122,163,1)')
+            getRoot.style.setProperty('--cor-analoga-a','rgba(44,59,87,1)')
+            getRoot.style.setProperty('--cor-analoga-b','rgba(170,195,240,1)')
+            getRoot.style.setProperty('--cor-contraste-a','rgba(87,71,36,1)')
+            getRoot.style.setProperty('--cor-contraste-b','rgba(163,143,100,1)')
+            break;
+        case 'lightStateGray':
+            getRoot.style.setProperty('--cor-base', 'rgba(125,150,170,1)')
+            getRoot.style.setProperty('--cor-analoga-a','rgba(60,79,94,1)')
+            getRoot.style.setProperty('--cor-analoga-b','rgba(208,230,247,1)')
+            getRoot.style.setProperty('--cor-contraste-a','rgba(94,77,51,1)')
+            getRoot.style.setProperty('--cor-contraste-b','rgba(171,153,126,1)')
+            break;
+        case 'spaceCadet':
+            getRoot.style.setProperty('--cor-base', 'rgba(43,45,66,1)')
+            getRoot.style.setProperty('--cor-analoga-a','rgba(79,84,143,1)')
+            getRoot.style.setProperty('--cor-analoga-b','rgba(107,110,143,1)')
+            getRoot.style.setProperty('--cor-contraste-a','rgba(143,126,64,1)')
+            getRoot.style.setProperty('--cor-contraste-b','rgba(66,61,43,1)')
+    } 
 }
