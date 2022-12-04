1ae28fe663da08372a7833063933dfce4ab29d70
CalebeCopello <calebecopello@gmail.com>
2022-12-03 23:44:41 -0300

    Temas
    Atualização de temas

--- js/script.js
+++ js/script.js
@@ -21,28 +21,34 @@ function show(a) {
             document.getElementById('contato').style.display = "inline";
     }
 }
+//mudar a cor do icone, escuro para os dois primeiros, claro para o ultimo
 function tema(tema) {
     var getRoot = document.querySelector(':root')
+    var icone = document.querySelector('.iconecabeca')
     switch(tema){
-        case 'glaucous':
-            getRoot.style.setProperty('--cor-base', 'rgba(100,122,163,1)')
-            getRoot.style.setProperty('--cor-analoga-a','rgba(44,59,87,1)')
-            getRoot.style.setProperty('--cor-analoga-b','rgba(170,195,240,1)')
-            getRoot.style.setProperty('--cor-contraste-a','rgba(87,71,36,1)')
-            getRoot.style.setProperty('--cor-contraste-b','rgba(163,143,100,1)')
-            break;
         case 'lightStateGray':
             getRoot.style.setProperty('--cor-base', 'rgba(125,150,170,1)')
             getRoot.style.setProperty('--cor-analoga-a','rgba(60,79,94,1)')
             getRoot.style.setProperty('--cor-analoga-b','rgba(208,230,247,1)')
             getRoot.style.setProperty('--cor-contraste-a','rgba(94,77,51,1)')
             getRoot.style.setProperty('--cor-contraste-b','rgba(171,153,126,1)')
+            icone.style.filter="invert(0) opacity(60%)"
             break;
-        case 'spaceCadet':
-            getRoot.style.setProperty('--cor-base', 'rgba(43,45,66,1)')
-            getRoot.style.setProperty('--cor-analoga-a','rgba(79,84,143,1)')
-            getRoot.style.setProperty('--cor-analoga-b','rgba(107,110,143,1)')
-            getRoot.style.setProperty('--cor-contraste-a','rgba(143,126,64,1)')
-            getRoot.style.setProperty('--cor-contraste-b','rgba(66,61,43,1)')
+        case 'sage':
+            getRoot.style.setProperty('--cor-base', 'rgba(202,202,170,1)')
+            getRoot.style.setProperty('--cor-analoga-a','rgba(117,117,117,1)')
+            getRoot.style.setProperty('--cor-analoga-b','rgba(255,255,229,1)')
+            getRoot.style.setProperty('--cor-contraste-a','rgba(102,80,125,1)')
+            getRoot.style.setProperty('--cor-contraste-b','rgba(185,169,201,1)')
+            icone.style.filter="invert(0) opacity(60%)"
+            break;
+        case 'onyx':
+            getRoot.style.setProperty('--cor-base', 'rgba(69,69,69,1)')
+            getRoot.style.setProperty('--cor-analoga-a','rgba(42, 42, 42,1)')
+            getRoot.style.setProperty('--cor-analoga-b','rgba(153,153,153,1)')
+            getRoot.style.setProperty('--cor-contraste-a','rgba(145,135,116,1)')
+            getRoot.style.setProperty('--cor-contraste-b','rgba(68,79,107,1)')
+            var icone = document.querySelector('.iconecabeca')
+            icone.style.filter="invert(1) opacity(60%)"
     } 
 }
