1ab375322a03e79c1d638520728bfa415b52a87a
CalebeCopello <calebecopello@gmail.com>
2022-12-13 10:07:13 -0300

    Burguer Menu
    Implementação do menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function show(a) {
-    document.getElementById('home').style.display = "none"
-    document.getElementById('sobre').style.display = "none"
-    document.getElementById('post').style.display = "none"
-    document.getElementById('projeto').style.display = "none"
-    document.getElementById('contato').style.display = "none"
+    document.getElementById('home').style.display = "none";
+    document.getElementById('sobre').style.display = "none";
+    document.getElementById('post').style.display = "none";
+    document.getElementById('projeto').style.display = "none";
+    document.getElementById('contato').style.display = "none";
     switch(a) {
         case 'home':
             document.getElementById('home').style.display = "inline";
@@ -22,32 +22,44 @@ function show(a) {
     }
 }
 function tema(tema) {
-    var getRoot = document.querySelector(':root')
-    var icone = document.querySelector('.iconecabeca')
+    var getRoot = document.querySelector(':root');
+    var icone = document.querySelector('.iconecabeca');
     switch(tema){
         case 'lightStateGray':
-            getRoot.style.setProperty('--cor-base', 'rgba(125,150,170,1)')
-            getRoot.style.setProperty('--cor-analoga-a','rgba(60,79,94,1)')
-            getRoot.style.setProperty('--cor-analoga-b','rgba(208,230,247,1)')
-            getRoot.style.setProperty('--cor-contraste-a','rgba(94,77,51,1)')
-            getRoot.style.setProperty('--cor-contraste-b','rgba(171,153,126,1)')
-            icone.style.filter="invert(0) opacity(60%)"
+            getRoot.style.setProperty('--cor-base', 'rgba(125,150,170,1)');
+            getRoot.style.setProperty('--cor-analoga-a','rgba(60,79,94,1)');
+            getRoot.style.setProperty('--cor-analoga-b','rgba(208,230,247,1)');
+            getRoot.style.setProperty('--cor-contraste-a','rgba(94,77,51,1)');
+            getRoot.style.setProperty('--cor-contraste-b','rgba(171,153,126,1)');
+            icone.style.filter="invert(0) opacity(60%)";
             break;
         case 'sage':
-            getRoot.style.setProperty('--cor-base', 'rgba(202,202,170,1)')
-            getRoot.style.setProperty('--cor-analoga-a','rgba(117,117,117,1)')
-            getRoot.style.setProperty('--cor-analoga-b','rgba(255,255,229,1)')
-            getRoot.style.setProperty('--cor-contraste-a','rgba(102,80,125,1)')
-            getRoot.style.setProperty('--cor-contraste-b','rgba(185,169,201,1)')
-            icone.style.filter="invert(0) opacity(60%)"
+            getRoot.style.setProperty('--cor-base', 'rgba(202,202,170,1)');
+            getRoot.style.setProperty('--cor-analoga-a','rgba(117,117,117,1)');
+            getRoot.style.setProperty('--cor-analoga-b','rgba(255,255,229,1)');
+            getRoot.style.setProperty('--cor-contraste-a','rgba(102,80,125,1)');
+            getRoot.style.setProperty('--cor-contraste-b','rgba(185,169,201,1)');
+            icone.style.filter="invert(0) opacity(60%)";
             break;
         case 'onyx':
-            getRoot.style.setProperty('--cor-base', 'rgba(69,69,69,1)')
-            getRoot.style.setProperty('--cor-analoga-a','rgba(42, 42, 42,1)')
-            getRoot.style.setProperty('--cor-analoga-b','rgba(153,153,153,1)')
-            getRoot.style.setProperty('--cor-contraste-a','rgba(145,135,116,1)')
-            getRoot.style.setProperty('--cor-contraste-b','rgba(68,79,107,1)')
-            var icone = document.querySelector('.iconecabeca')
-            icone.style.filter="invert(1) opacity(60%)"
+            getRoot.style.setProperty('--cor-base', 'rgba(69,69,69,1)');
+            getRoot.style.setProperty('--cor-analoga-a','rgba(42, 42, 42,1)');
+            getRoot.style.setProperty('--cor-analoga-b','rgba(153,153,153,1)');
+            getRoot.style.setProperty('--cor-contraste-a','rgba(145,135,116,1)');
+            getRoot.style.setProperty('--cor-contraste-b','rgba(68,79,107,1)');
+            var icone = document.querySelector('.iconecabeca');
+            icone.style.filter="invert(1) opacity(60%)";
     } 
+}
+function menu() {
+    var getMenu = document.querySelector('menu > ul');
+    var getMenuIcon = document.getElementById('iconemenu');
+    if (getMenu.style.visibility == 'visible' || getMenu.style.visibility == '') {
+        getMenu.style.visibility = "hidden";
+        getMenuIcon.src="/imgs/menu-burger-from-freepik.png";
+    }
+    else {
+        getMenu.style.visibility = "visible";
+        getMenuIcon.src="/imgs/cross-from-freepik.png"
+    }
 }
